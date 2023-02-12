8343c8a81aae2b5c51a19dfa7d18e7d09969d5bf
 NohYeongHun <dkzndk1100@naver.com>
2023-02-12 22:59:52 +0900

    기존 IoC, DI 컨테이너의 역할을 스프링 컨테이너로 전환 - AppConfig 를 스프링 컨테이너에 Configuration 으로 등록한다. - AppConfig 의 Bean 들을 등록한다. - Spring Container 는 ApplicationContext 객체로 관리된다. - Spring Container 에서 AppConfig 에 등록한 Bean 객체들을 관리한다. - 프로그래머는 의존성에 대한 관리가 훨씬 수월해진다.

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+        //AppConfig appConfig = new AppConfig();
         //MemberService memberService = new MemberServiceImpl();
-        MemberService memberService = appConfig.memberService();
+
+        // AppConfig 에서 생성하는 Bean 객체들을 모두 스프링 컨테이너에서 관리함.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 기본적으로 Bean 이름은 Method() 이름으로 저장된다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
