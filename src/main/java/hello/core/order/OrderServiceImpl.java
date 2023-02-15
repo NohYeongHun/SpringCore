92118c1f39bc306833d258f196fe7b33119cff21
 NohYeongHun <dkzndk1100@naver.com>
2023-02-16 08:50:30 +0900

    수정자 주입 - @AutoWired 가 걸린 setMethod()에 의존성이 자동으로 주입된다. - @AutoWired 의 기본 동작은 주입할 대상이 없으면 오류가 발생함. - 생성자 주입은 객체를 생성해서 빈에 등록해야될 때 생성자를 호출해야 하므로 @Autowired 가 붙어 있으면 의존성에 해당하는 값들을 스프링 빈에서 찾아와서 주입하고 객체를 생성한다. - 수정자 주입은 객체가 생성되고 난뒤 @Autowired 가 걸린 setMethod 에 의존성에 해당하는 값이 주입된다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -30,6 +30,7 @@ public class OrderServiceImpl implements OrderService{
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("1. OrderServiceImpl : 호출 ");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
